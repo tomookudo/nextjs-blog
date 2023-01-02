116a4e893610c3a3f335dfdd702bc0c0a1acea1f
kudo <kou227227@gmail.com>
2023-01-02 10:06:27 +0900

    #5-2 終了

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,21 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+// ### Pre-rendering(ServerSide, Static Generation) -- only available under /pages dir.
+export async function getStaticProps() {   
+// ### Server Side Rendering  -- only available under /pages dir.
+// export async function getServerSideProps(context) { 
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home ({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -16,6 +29,20 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
         go to <Link href="/posts/first-post">first-post</Link>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
